b7a7a066c915a46315d63391a5fc3c6a933c81e4
Noman Ali <nomi4698dg@gmail.com>
2025-08-26 02:07:24 +0500

    hello

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 export default function setupSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: process.env.FRONTEND_URL || 'http://localhost:5173/',
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000/',
       methods: ['GET', 'POST']
     }
   });
